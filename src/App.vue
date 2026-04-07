d61bae8dcd1627127774d2fbe897ebc0d397e299
Sh3hab <banshihab2025@gmail.com>
2026-04-08 02:41:25 +0300

    base

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import UserCard from './components/UserCard.vue'
+import Tags from './components/Tags.vue'
+</script>
+
+
+<template>
+  <Tags/>
+  <HelloWorld />
+  <section id="user-info">
+  <UserCard 
+    name="شهاب بندر" 
+    role="مصمم واجهات" 
+    status="نشط" 
+    
+  />
+  <UserCard 
+    name="علي توفيق" 
+    role="مصمم تجربة مستخدم" 
+    status="غير متصل" 
+  />
+</section>
+
+
+</template>
